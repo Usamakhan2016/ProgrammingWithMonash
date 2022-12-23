31798bce4b73a58ff32ce875762d70a214576be4
UsamaKhan2016 <engrmuhammadusama@gmail.com>
2022-12-23 21:09:14 +0500

    C# Fundamentals

--- Program.cs
+++ Program.cs
@@ -12,6 +12,26 @@ namespace ProgrammingWithMonash
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Usama Sakhawat");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
+
+            var lastName = "Hammadani";
+
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(lastName);
+
+            Console.WriteLine("{0} {1}",byte.MinValue,byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         }
     }
 }
